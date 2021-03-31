9ede303d7d1d93b7936ce2a78d9628a806e80328
Patrick Lynch <pajlynch@gmail.com>
2021-03-31 23:08:19 +0000

    Automatically backed up by Learn

--- times.rb
+++ times.rb
@@ -1,7 +1,3 @@
-jewels_in_bag = 100
-
-3.times do
-  puts "Hiding 10 stolen jewels."
-  jewels_in_bag = jewels_in_bag - 10
-  puts "Now there are only #{jewels_in_bag} jewels left to hide!"
+def using_times
+	#your code here
 end
